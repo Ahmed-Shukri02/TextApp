6060f4b2249c433a65e9a29b1a9877373fa52046
Ahmed-Shukri02 <ahmed.s100402@gmail.com>
2022-08-05 19:15:55 +0100

    initialised cypress for e2e testing

---
+++ cypress/e2e/user_post.js
@@ -0,0 +1,16 @@
+describe("post", () =>{
+  it("user can log in, navigate to another users page, like a post and reply to the post", () => {
+    // user lands at home and navigates to login page
+    // NAVIGATE TO localhost:3000/login
+
+    // log in using the brain 3 account
+    // WRITE INTO EMAIL AND PASSWORD AND CLICK LOGIN
+
+    // navigate to another users page
+    // NAVIGATE TO localhost:3000/users/brain THEN CLICK HOME
+
+    //
+
+
+  })
+})
