16e9dea1d6fa73f6bc41e5cd338840a67e7c7350
Ahmed-Shukri02 <ahmed.s100402@gmail.com>
2022-08-25 12:11:57 +0100

    changed environment variables

--- client/src/components/profileLink.js
+++ client/src/components/profileLink.js
@@ -2,16 +2,38 @@ import React from "react";
 import IconComponents from "../icon-components/icon-components";
 import Buttons from "./Buttons/Buttons";
 
-export default function profileLink({fromFeed, feedHandleClickOff}){
-  return(
+export default function profileLink({ fromFeed, feedHandleClickOff }) {
+  return (
     <div className="profile-link">
-      <Buttons.DefaultButton width="100%" height="2em" contentColor="white" fontSize="0.9rem">
-        <div style={{display: "flex", alignItems: "center", gap: "0.5em", justifyContent: "center"}}>
-          <IconComponents.BellIcon/>
+      <Buttons.DefaultButton
+        width="100%"
+        height="2em"
+        contentColor="white"
+        fontSize="0.9rem"
+      >
+        <div
+          style={{
+            display: "flex",
+            alignItems: "center",
+            gap: "0.5em",
+            justifyContent: "center",
+          }}
+        >
+          <IconComponents.BellIcon />
           <span> Follow </span>
         </div>
       </Buttons.DefaultButton>
-      {fromFeed && <div style={{display:"flex", justifyContent:"center"}}><Buttons.UnderlineButton handleClick={feedHandleClickOff} contentColor="red" fontSize="0.8rem">Close Tab</Buttons.UnderlineButton></div>}
+      {fromFeed && (
+        <div style={{ display: "flex", justifyContent: "center" }}>
+          <Buttons.UnderlineButton
+            handleClick={feedHandleClickOff}
+            contentColor="red"
+            fontSize="0.8rem"
+          >
+            Close Tab
+          </Buttons.UnderlineButton>
+        </div>
+      )}
     </div>
-  )
+  );
 }
